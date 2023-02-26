1be2a2ae5bb9f589be729dcef5df27975d33a69e
Dominique Eclavia <dominiquealvarez8888@gmail.com>
2023-02-27 00:55:36 +0800

    dashboard content

--- script.js
+++ script.js
@@ -60,3 +60,23 @@ function showNavBar(){
     }
 }
 
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: '# of Votes',
+    data: [102, 19, 3, 5, 2, 3],
+    borderWidth: 1
+    }]
+},
+options: {
+    scales: {
+    y: {
+        beginAtZero: true
+    }
+    }
+}
+});
